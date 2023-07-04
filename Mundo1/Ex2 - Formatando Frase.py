942dfc0ce08c72ec13e982d3d34bb36cea015f50
Thiago Oliveira <geovanithiago08@gmail.com>
2023-07-03 21:39:58 -0300

    Ex 2 - Formatando Frase
    Aprendendo o comando .format em python

---
+++ Mundo1/Ex2 - Formatando Frase.py
@@ -0,0 +1,10 @@
+#Desafio 02 Curso em video ep4
+
+
+data= input ("Digite o dia que nasceu:" )
+mes = input ("Digite o Mês que nasceu: ")
+ano = input ("Digite o ano que nasceu:" )
+
+#print("Você nasceu no dia", data,"do Mês de", mes,  "do Ano de", ano , "Seja Bem-vindo")
+print('Voce nasceu no dia {} do mes {} do ano de {} , Seja Bem dindo! '.format(data, mes, ano))
+#.format e usado para colocar os valores nas chaves na ordem. ex. o primeiro valor a ser mostrado e data.   
